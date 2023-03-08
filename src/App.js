e78f48a7f332b2f47d8a1ccd907304bbba38bbce
rostikKalabiska <rostik31052002@gmai.com>
2023-03-08 19:45:32 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Categories } from "./components/Categories/Categories";
+import { Header } from "./components/Header/Header";
+import { PizzaBlock } from "./components/PizzaBlock/PizzaBlock";
+import { Sort } from "./components/Sort";
+import "./scss/app.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">All pizza</h2>
+          <div class="content__items"></div>
+          <PizzaBlock title="dsadsada" price={29.99} />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,14 @@
+export function Categories() {
+  return (
+    <div className="categories">
+      <ul>
+        <li className="active">All</li>
+        <li>Meat</li>
+        <li>Vegetarian</li>
+        <li>Grill</li>
+        <li>Spicy</li>
+        <li>Closed</li>
+      </ul>
+    </div>
+  );
+}
